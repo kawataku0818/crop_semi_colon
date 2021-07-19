128457beffe138e244f33af814ef327bff78e0e1
takushi kawata <takushi.kawata@tokai-soft.co.jp>
2021-07-19 14:35:03 +0900

    new

---
+++ crop_semi_colon/crop_semi_colon/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace crop_semi_colon
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.hSmartWindowControl1 = new HalconDotNet.HSmartWindowControl();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(116, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "read_image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(135, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(653, 19);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Symbol.png";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(117, 40);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "roi";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // hSmartWindowControl1
+            // 
+            this.hSmartWindowControl1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.hSmartWindowControl1.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
+            this.hSmartWindowControl1.HDoubleClickToFitContent = true;
+            this.hSmartWindowControl1.HDrawingObjectsModifier = HalconDotNet.HSmartWindowControl.DrawingObjectsModifier.None;
+            this.hSmartWindowControl1.HImagePart = new System.Drawing.Rectangle(0, 0, 640, 480);
+            this.hSmartWindowControl1.HKeepAspectRatio = true;
+            this.hSmartWindowControl1.HMoveContent = true;
+            this.hSmartWindowControl1.HZoomContent = HalconDotNet.HSmartWindowControl.ZoomContent.WheelForwardZoomsIn;
+            this.hSmartWindowControl1.Location = new System.Drawing.Point(135, 38);
+            this.hSmartWindowControl1.Margin = new System.Windows.Forms.Padding(0);
+            this.hSmartWindowControl1.Name = "hSmartWindowControl1";
+            this.hSmartWindowControl1.Size = new System.Drawing.Size(653, 403);
+            this.hSmartWindowControl1.TabIndex = 3;
+            this.hSmartWindowControl1.WindowSize = new System.Drawing.Size(653, 403);
+            this.hSmartWindowControl1.HMouseMove += new HalconDotNet.HMouseEventHandler(this.hSmartWindowControl1_HMouseMove);
+            this.hSmartWindowControl1.HMouseDown += new HalconDotNet.HMouseEventHandler(this.hSmartWindowControl1_HMouseDown);
+            this.hSmartWindowControl1.HMouseUp += new HalconDotNet.HMouseEventHandler(this.hSmartWindowControl1_HMouseUp);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 88);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 71);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "segment character\r\nselect character\r\n";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 165);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(117, 27);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "replace";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.hSmartWindowControl1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private HalconDotNet.HSmartWindowControl hSmartWindowControl1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ crop_semi_colon/crop_semi_colon/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using HalconDotNet;
+
+namespace crop_semi_colon
+{
+    public partial class Form1 : Form
+    {
+        HObject _image;
+        bool _flg;
+        bool _flg1;
+        double _x;
+        double _y;
+        HObject _rectangle;
+        HObject _regionCharacters;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            hSmartWindowControl1.MouseWheel += hSmartWindowControl1.HSmartWindowControl_MouseWheel;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HOperatorSet.ReadImage(out _image, textBox1.Text);
+            hSmartWindowControl1.HalconWindow.DispObj(_image);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _flg = true;
+            hSmartWindowControl1.HMoveContent = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            HOperatorSet.ReduceDomain(_image, _rectangle, out HObject imageReduced);
+            HTuple widthAve = new HTuple(50, 1, 150);
+            HTuple heightAve = new HTuple(50, 1, 150);
+            HTuple contrast = 150;
+            HOperatorSet.SegmentCharacters(imageReduced, imageReduced, out HObject imageForeground,
+                out HObject regionForeground, "local_contrast_best", "false", "false", "medium",
+                widthAve, heightAve, 0, contrast, out HTuple usedThreshold);
+            HTuple connectFragments = "true";
+            HTuple flagmentDistance = "medium";
+            //HTuple flagmentDistance = "wide";
+            HTuple charWidth = widthAve;
+            HTuple charHeight = heightAve;
+            HOperatorSet.SelectCharacters(regionForeground, out _regionCharacters,
+                "false", "medium", charWidth, charHeight, "true", "false", "variable_width", "false", flagmentDistance,
+                connectFragments, 0, "completion");
+
+            hSmartWindowControl1.HalconWindow.DispObj(_image);
+            hSmartWindowControl1.HalconWindow.SetColored(12);
+            hSmartWindowControl1.HalconWindow.DispObj(_regionCharacters);
+
+        }
+
+
+
+        private void hSmartWindowControl1_HMouseDown(object sender, HMouseEventArgs e)
+        {
+            _flg1 = true;
+            _x = e.X;
+            _y = e.Y;
+        }
+
+        private void hSmartWindowControl1_HMouseMove(object sender, HMouseEventArgs e)
+        {
+            if (_image == null)
+            {
+                return;
+            }
+            if (!_flg)
+            {
+                return;
+            }
+            if (!_flg1)
+            {
+                return;
+            }
+            HOperatorSet.GenRectangle1(out _rectangle, _y, _x, e.Y, e.X);
+            hSmartWindowControl1.HalconWindow.DispObj(_image);
+            hSmartWindowControl1.HalconWindow.SetDraw("margin");
+            hSmartWindowControl1.HalconWindow.SetColor("red");
+            hSmartWindowControl1.HalconWindow.DispObj(_rectangle);
+        }
+
+        private void hSmartWindowControl1_HMouseUp(object sender, HMouseEventArgs e)
+        {
+            _flg = false;
+            _flg1 = false;
+            hSmartWindowControl1.HMoveContent = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // : ; 対策で領域を縦に拡張する
+            HOperatorSet.SortRegion(_regionCharacters, out _regionCharacters, "first_point", "true", "column");
+            HOperatorSet.CountObj(_regionCharacters, out HTuple number);
+            HTuple oldColumns = new HTuple();
+            HOperatorSet.GenEmptyObj(out HObject oldRegion);
+            HTuple oldIndex = 0;
+            HOperatorSet.GenEmptyObj(out HObject newRegion);
+            for (int i = 1; i <= number; i++)
+            {
+                HOperatorSet.SelectObj(_regionCharacters, out HObject objectSelected, i);
+                HOperatorSet.GetRegionPoints(objectSelected, out HTuple __, out HTuple columns);
+                if (i == 1)
+                {
+                    oldColumns = columns;
+                    HOperatorSet.CopyObj(objectSelected, out oldRegion, 1, 1);
+                    oldIndex = i;
+                    continue;
+                }
+                bool flg = false;
+                for (int j = 0; j < oldColumns.Length; j++)
+                {
+                    for (int k = 0; k < columns.Length; k++)
+                    {
+                        HOperatorSet.TupleEqual(oldColumns[j], columns[k], out HTuple equal);
+                        if (equal)
+                        {
+                            HOperatorSet.Union2(oldRegion, objectSelected, out HObject regionUnion);
+                            HOperatorSet.ConcatObj(newRegion, regionUnion, out newRegion);
+                            i++;
+                            flg = true;
+                            if (i <= number)
+                            {
+                                HOperatorSet.SelectObj(_regionCharacters, out objectSelected, i);
+                                HOperatorSet.ConcatObj(newRegion, objectSelected, out newRegion);
+                            }
+                            break;
+                        }
+                    }
+                    if (flg)
+                    {
+                        break;
+                    }
+                }
+                if (!flg)
+                {
+                    // 結合なし
+                    HOperatorSet.ConcatObj(newRegion, oldRegion, out newRegion);
+                }
+                if (!flg && i == number)
+                {
+                    HOperatorSet.ConcatObj(newRegion, objectSelected, out newRegion);
+                }
+                oldColumns = columns;
+                HOperatorSet.CopyObj(objectSelected, out oldRegion, 1, 1);
+                oldIndex = i;
+
+                // for debug
+                //HOperatorSet.GenImageConst(out HObject image, "byte", 1024, 512);
+                //HOperatorSet.OverpaintRegion(image, objectSelected, 255, "fill");
+                //HOperatorSet.WriteImage(image, "tiff", 0, "aaa.tif");
+            }
+
+            // for debug
+            //HOperatorSet.CountObj(newRegion, out HTuple nubm);
+
+            HOperatorSet.SortRegion(newRegion, out HObject sortedRegions, "character", "true", "row");
+
+            // 表示
+            hSmartWindowControl1.HalconWindow.DispObj(_image);
+            hSmartWindowControl1.HalconWindow.DispObj(sortedRegions);
+        }
+    }
+}
